bd577fc945751ae36f8549c693982b3488844849
tyland-ti <triview@naver.com>
2022-03-06 19:46:09 +0900

    native query

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -68,4 +68,7 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
 
     <T> List<T> findProjectionsByUsername(@Param("username") String username, Class<T> type);
 
+    @Query(value = "select * from Member where username = ?", nativeQuery = true)
+    Member findByNativeQuery(String username);
+
 }
